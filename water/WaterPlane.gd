fc5f60d1c7cffe0ace379326414eb096dd89800e
Kepka Ludovic <shionn@gmail.com>
2025-11-01 13:33:35 +0100

    des truc

--- water/WaterPlane.gd
+++ water/WaterPlane.gd
@@ -6,4 +6,5 @@ func set_perspective(perspective: bool) -> void:
 	if material:
 		# When changing between ortho/perspective, the far value changes
 		# We must adjust our edge threshold to reflect this, or the shader will not work well
-		material.set_shader_parameter("edge_scale", 0.5 if perspective else 0.00001);
+		material.set_shader_parameter("edge_scale", 0.5);
+		#material.set_shader_parameter("")
