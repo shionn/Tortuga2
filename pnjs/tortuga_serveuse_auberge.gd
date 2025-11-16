ce7a1e66353d6169449d2c61018ad6f8009e1a3f
Kepka Ludovic <shionn@gmail.com>
2025-11-16 22:43:05 +0100

    Un premier pnj

---
+++ pnjs/tortuga_serveuse_auberge.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@onready var _animation := $character/AnimationPlayer as AnimationPlayer
+
+func _ready() -> void:
+	_animation.get_animation("idle").loop_mode = Animation.LOOP_LINEAR
+	_animation.play("idle")
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_activation_mouse_entered() -> void:
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
+func _on_activation_mouse_exited() -> void:
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+
+func _on_activation_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and Input.is_action_just_pressed("interact"): 
+		print("action")
