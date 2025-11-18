fbf68bb880b478b18ed43e17a68e1160c07c8c76
Kepka Ludovic <shionn@gmail.com>
2025-11-18 22:22:10 +0100

    pnj

--- pnjs/pnj.gd
+++ pnjs/pnj.gd
@@ -4,23 +4,17 @@ extends Node3D
 @onready var _name_3d := $name as Label3D
 @onready var _gui := $"/root/World/Gui" as Control
 @onready var _player := $"/root/World/Player" as Node3D
-@onready var _player_camera := $"/root/World/Player/CameraPivot" as Node3D
 
-const _name := "Maya"
-const _dialog := """Bonjour aventurier,
-
-Je vous sert quelque chose à boire ?
-"""
-
+var _pnj_name := "not set"
+var _pnj_dialog := "not set"
 
 func _ready() -> void:
-	_name_3d.text = _name
+	_name_3d.text = _pnj_name
 	_animation.get_animation("idle").loop_mode = Animation.LOOP_LINEAR
 	_animation.play("idle")
 
 func _process(delta: float) -> void:
 	_name_3d.look_at(_player.global_position, Vector3.UP, true)
-	#_name_3d.set_global_rotate(Vector3.UP, _player_camera.global_rotation.y)
 
 func _on_activation_mouse_entered() -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
@@ -30,4 +24,4 @@ func _on_activation_mouse_exited() -> void:
 
 func _on_activation_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton and Input.is_action_just_pressed("interact"): 
-		_gui.openDialog(_name, _dialog)
+		_gui.openDialog(_pnj_name, _pnj_dialog)
---
+++ pnjs/tortuga/serveurse-auberge-maya.gd
@@ -0,0 +1,5 @@
+extends "res://pnjs/pnj.gd"
+
+func _init() -> void:
+	_pnj_name = "Maya"
+	_pnj_dialog = """Ceci est dialog depuis un pnj generique"""
