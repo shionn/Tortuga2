ef9f40239d1ebc4f7c47c076f63e87eb0e2242cb
Shionn <shionn@gmail.com>
2025-11-18 13:58:24 +0100

    Petit retour sur le pnj

--- gui/gui.gd
+++ gui/gui.gd
@@ -4,16 +4,17 @@ extends Control
 @onready var _debug_info := $DebugInfo as Label
 @onready var _dialog := $Left/Dialog as Panel
 @onready var _dialog_text := $Left/Dialog/MarginContainer/VBoxContainer/RichTextLabel as RichTextLabel
+@onready var _dialog_title := $Left/Dialog/MarginContainer/VBoxContainer/Title as Label
 
-func openDialog(text: String) -> void: 
+func openDialog(title: String, text: String) -> void: 
 	_dialog_text.clear()
 	_dialog_text.add_text(text)
+	_dialog_title.text = title
 	_dialog.show()
 
 func _process(delta: float) -> void:
 	_debug_info.text = str(_player.global_position)
 	_debug_info.text += "\nfps: " + str(Engine.get_frames_per_second())
-
 
 func _on_close_dialog_button_pressed() -> void:
 	_dialog.hide()
--- pnjs/tortuga_serveuse_auberge.gd
+++ pnjs/tortuga_serveuse_auberge.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
 @onready var _animation := $character/AnimationPlayer as AnimationPlayer
+@onready var _name_3d := $name as Label3D
 @onready var _gui := $"/root/World/Gui" as Control
+@onready var _player := $"/root/World/Player" as Node3D
+@onready var _player_camera := $"/root/World/Player/CameraPivot" as Node3D
 
+const _name := "Maya"
 const _dialog := """Bonjour aventurier,
 
 Je vous sert quelque chose à boire ?
@@ -10,11 +14,13 @@ Je vous sert quelque chose à boire ?
 
 
 func _ready() -> void:
+	_name_3d.text = _name
 	_animation.get_animation("idle").loop_mode = Animation.LOOP_LINEAR
 	_animation.play("idle")
 
 func _process(delta: float) -> void:
-	pass
+	_name_3d.look_at(_player.global_position, Vector3.UP, true)
+	#_name_3d.set_global_rotate(Vector3.UP, _player_camera.global_rotation.y)
 
 func _on_activation_mouse_entered() -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
@@ -24,4 +30,4 @@ func _on_activation_mouse_exited() -> void:
 
 func _on_activation_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton and Input.is_action_just_pressed("interact"): 
-		_gui.openDialog(_dialog)
+		_gui.openDialog(_name, _dialog)
