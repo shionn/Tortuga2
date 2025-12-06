e2470b68cbd38b99d7b112d7a391d31899dab8a7
Kepka Ludovic <shionn@gmail.com>
2025-12-06 23:12:57 +0100

    Note dans les bouteilles

---
+++ scripts/tortuga/bouteille_note_lisible.gd
@@ -0,0 +1,13 @@
+extends "res://scripts/interactable.gd"
+
+func on_interact() -> void:
+	open_text("Bouteille", """Cette bouteille contient une note. 
+
+Bravo tu es valeureux et malin,
+Mais cela ne suffira pas pour arpenter le chemin,
+Robuste tu dois être,
+Pour atteindre le Haut Mat qui trône au dessus de l'ile,
+Lui qui surveille si des êtres villes,
+Du butin voudraient s'emparer,
+Mais garde le chemin à ses pieds.
+""")
