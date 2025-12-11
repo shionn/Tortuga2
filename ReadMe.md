dd726e9abfadb0e0a5d37848b40b0ff23b89e9b8
Kepka Ludovic <shionn@gmail.com>
2025-12-11 23:09:00 +0100

    Manoir

--- ReadMe.md
+++ ReadMe.md
@@ -6,3 +6,4 @@
 
 - https://opengameart.org/content/rpg-item-collection-2
 - https://kenney.nl/assets
+- https://opengameart.org/content/opendungeons-barbarian-throne
---
+++ scripts/tortuga/pnj-manoir-majordhomme.gd
@@ -0,0 +1,9 @@
+extends "res://scripts/pnj.gd"
+
+func _init() -> void:
+	_pnj_name = "Majordome"
+	_pnj_dialog = """Bonjour. 
+	
+	Whisp est en haut il t'attends.
+	
+	TODO a revoir"""
