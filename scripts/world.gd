6dfc7fd44882ea7a6e8401830d0aebd2cfdcddee
Kepka Ludovic <shionn@gmail.com>
2025-12-12 23:34:51 +0100

    definition poitn de depart

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+@onready var _player_pivot = $Player/Pivot
+@onready var _player_camera = $Player/CameraPivot
+
 func _on_ready() -> void:
-	
+	_player_pivot.rotate_y(deg_to_rad(165))
+	_player_camera.rotate_y(deg_to_rad(165))
 	print("ready")
