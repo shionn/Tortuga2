f124f1a4b2881ba9d02d340a8035cc54f80758d9
Kepka Ludovic <shionn@gmail.com>
2025-12-13 10:46:23 +0100

    Ajout alchimliste

---
+++ scripts/tortuga/pnj-brasserie-alchimiste-adan.gd
@@ -0,0 +1,7 @@
+extends "res://scripts/pnj.gd"
+
+func on_interact() -> void:
+	open_text(pnj_name, """Bonjour, 
+
+Je suis Adan l'achimiste de l'île, je sais faire de puissante potion. 
+Mais je suis surtout connu pour ma bière et mon rhum.""")
