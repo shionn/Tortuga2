1b6a37c9d7e203e5d00a0716226b03ca03836b58
Kepka Ludovic <shionn@gmail.com>
2025-12-13 11:50:46 +0100

    Ajou pnj orco

---
+++ scripts/tortuga/pnj-armurerie-orco.gd
@@ -0,0 +1,8 @@
+extends "res://scripts/pnj.gd"
+
+func on_interact() -> void:
+	open_text(pnj_name, """Bonjour. 
+
+Mon nom est imprononçable pour vous les humains. Vos semblables m'appellent Orco, je suis l'armurier du village. 
+
+Actuellement l'armurerie est fermée, nous ouvrons quand les tambours de guerre résonnent.""")
