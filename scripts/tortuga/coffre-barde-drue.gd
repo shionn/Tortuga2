9c88de901957b064e6834a49546f3c00cfffaf6d
Kepka Ludovic <shionn@gmail.com>
2025-12-13 10:22:06 +0100

    quelque fix de dialog

--- scripts/tortuga/coffre-barde-drue.gd
+++ scripts/tortuga/coffre-barde-drue.gd
@@ -7,13 +7,14 @@ func on_interact() -> void:
 
 func on_answer(answer: String) -> void:
 	if answer == "14738":
-		open_dialog_and_img("Félicitation", """Vous avez résolut l'énigme du trésor de Brabre drue.
+		open_dialog_and_img("Félicitation", """Vous avez résolue l'énigme de Barbe Drue
 
-Dane le coffre en plus du trésor, vous trouver un parchemin, mais vous ne comprenez rien à ce qui est écris dessus. 
-Peut-être que quelqu'un au village saura qu'en faire. 
+Dans ce coffre en plus t'as amas d'or, et de rubis vous trouverez également un parchemin,
+Mais de texte anciens n'étant pas un érudit,
+De ce que vous lisez vous n'y comprenez rien,
+Peut être qu'au village,
+Pour le déchiffrer vous trouverez un sage.
 
-Pour prouvez que vous avez fini cette quête reporter le code \"Par ma barbe rousse et drue\" dans le canal discord des quêtes.
-
-TODO Revoir ce dialog et cette image""", load("res://assets/imgs/treasures.png"))
+Pour prouvez que vous avez fini cette quête reporter le code \"Par ma barbe rousse et drue\" dans le canal discord des quêtes.""", load("res://assets/imgs/treasures.png"))
 	else :
 		open_alert("Mauvais code", "Le coffre ne s'ouvre pas.")
