1d47c939eff5cd3671253712d98ef1db2006d60c
Kepka Ludovic <shionn@gmail.com>
2025-12-13 13:30:48 +0100

    Ajout d'un call back yes/no

--- gui/gui.gd
+++ gui/gui.gd
@@ -16,10 +16,16 @@ extends Control
 @onready var _input_text := $Input/Panel/MarginContainer/VBoxContainer/LineEdit as LineEdit
 @onready var _input_title := $Input/Panel/MarginContainer/VBoxContainer/Title as Label
 
+@onready var _input_yn := $InputYesNo as Control
+@onready var _input_yn_text := $InputYesNo/Panel/MarginContainer/VBoxContainer/RichTextLabel as RichTextLabel
+@onready var _input_yn_title := $InputYesNo/Panel/MarginContainer/VBoxContainer/Title as Label
+
 @onready var _player_camera := $"../Player/CameraPivot" as Node3D
 @onready var _compass_arrow := $Compass/Arrow as TextureRect
 
 var callback : Callable
+var callback_no : Callable
+var callback_yes : Callable
 
 func open_dialog(title: String, text: String, size: Vector2 = Vector2(400,400)) -> void: 
 	_dialog.set_size(size)
@@ -45,6 +51,14 @@ func open_question(title: String, callback: Callable) -> void:
 	_input.show()
 	self.callback = callback
 
+func open_yes_no_question(title: String, question: String, on_no: Callable, on_yes: Callable) -> void:
+	_input_title.text = title
+	_input_yn_text.clear()
+	_input_yn_text.add_text(question)
+	self.callback_no = on_no
+	self.callback_yes = on_yes
+	_input_yn.show()
+
 func _process(delta: float) -> void:
 	_debug_info.text = str(_player.global_position)
 	_debug_info.text += "\nfps: " + str(Engine.get_frames_per_second())
@@ -56,7 +70,14 @@ func _on_close_dialog_button_pressed() -> void:
 func _on_close_dialog_and_img_button_pressed() -> void:
 	_dialog_img.hide()
 
-
 func _on_validate_input_button_pressed() -> void:
 	_input.hide()
 	callback.call(_input_text.text)
+
+func _on_no_button_pressed() -> void:
+	_input_yn.hide()
+	self.callback_no.call()
+
+func _on_yes_button_pressed() -> void:
+	_input_yn.hide()
+	self.callback_yes.call()
