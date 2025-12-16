9cd92abc40a0e3676ac4e370c3eb599e40bd52f2
Shionn <shionn@gmail.com>
2025-12-16 13:50:26 +0100

    premier refresh basic du sac

--- gui/bag.gd
+++ gui/bag.gd
@@ -1,9 +1,15 @@
 extends Control
 
+@onready var _grid = $Panel/MarginContainer/VBoxContainer/GridContainer as GridContainer
+
 func _on_visibility_changed() -> void:
 	if visible :
+		for child in _grid.get_children() : 
+			_grid.remove_child(child)
+		var b = TextureButton.new()
+		b.texture_normal = load("res://assets/gui/fanatisy-magic-set/bag-h-48.png")
+		_grid.add_child(b)
 		print("refresh bag content")
-	pass # Replace with function body.
 
 
 func _on_close_button_pressed() -> void:
