5ce2a1a786606de78b147a87e1b8f322a7c8de51
Kepka Ludovic <shionn@gmail.com>
2025-12-29 14:44:44 +0100

    Retour du jardinier

--- scripts/tortuga/pnj-fermier-nino.gd
+++ scripts/tortuga/pnj-fermier-nino.gd
@@ -4,7 +4,7 @@ extends "res://scripts/pnj.gd"
 func on_interact() -> void:
 	open_text(pnj_name, _TEXT)
 
-const _TEXT = """Bonjour aventurier.ère, je suis le seul fermier de l’île. Ne marche pas dans mes cultures. Si vous abîmez mes cultures, nous n’aurons plus de houblon pour que Eve puisse en faire de la bonne bière !
+const _TEXT = """Bonjour aventurier.ère, je suis le seul jardinier de l’île. Ne marche pas dans mes cultures. Si vous abîmez mes cultures, nous n’aurons plus de houblon pour que Eve puisse en faire de la bonne bière !
 
 Et si il n’y a plus de bière, nous courons vers une mutinerie certaine. 
 
