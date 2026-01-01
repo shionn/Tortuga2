169b911a40ce4f7b82b41f701f45897706f14fd4
Kepka Ludovic <shionn@gmail.com>
2026-01-01 20:46:14 +0100

    Correction de transfo

--- scripts/tortuga/manoir/pnj-manoir-majordhomme.gd
+++ scripts/tortuga/manoir/pnj-manoir-majordhomme.gd
@@ -4,4 +4,4 @@ extends "res://scripts/pnj.gd"
 func on_interact() -> void:
 	gui.open_dialog(pnj_name, """Salut Pirate. 
 	
-Le Capitaine Whisp est haut il t'attend il a une mission pour toi.""")
+Le Capitaine Whisp est en haut, il t'attend. Il a une mission pour toi.""")
