b565908f19132693b4ac69457b0ed095fc81c90b
Kepka Ludovic <shionn@gmail.com>
2026-01-03 08:42:52 +0100

    Reorganisation Tortuga

--- scripts/tortuga/hung-connut/stone-hung-connut.gd
+++ scripts/tortuga/hung-connut/stone-hung-connut.gd
@@ -1,7 +1,19 @@
 extends "res://scripts/interactable.gd"
 
 func on_interact() -> void:
-	if bag.contain(Bag.CarteHungConnut) :
-		gui.open_dialog("Todo","TODO")
-	else :
-		gui.open_alert("Gravure étrange", "Quel étrange gravure")
+	gui.open_alert("Marbre de Hung Connut", _TEXT)
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	visible = bag.contain(Bag.CarteHungConnut) or player.tags.have(Tags.TREASUR_HUNG_CONNUT_OPENED)
+		
+
+const _TEXT = """Là où le vent souffle très fort,
+tu devra monter,
+car pour approcher mon trésor
+il te faudra le mériter!
+Au Nord de l'île,
+d’un pas habile et hâté, 
+trouve le panneau mobile,
+car il va te mener,
+là où autrement tu ne saurais aller!"""
