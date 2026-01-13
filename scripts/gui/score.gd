359482d1faeefb137c1a2a87ed04eb8f18c56503
Kepka Ludovic <shionn@gmail.com>
2026-01-13 18:28:36 +0100

    min / max score

--- scripts/gui/score.gd
+++ scripts/gui/score.gd
@@ -13,11 +13,11 @@ func _ready() -> void:
 	
 func compute() -> void :
 	value = 0
-	# quete 1
+	# quete 1 : min 1, max 2
 	_item(Bag.TresorBarbeDrue, 1)
 	_item(Bag.TresorBarbeDrueNet, 2) # remplace le TresorBarbeDrue
 	
-	# quete 2
+	# quete 2 : min 5, max 7
 	_tag(Tags.BRASSERIE_HAVE_SOBERING_POTION, 1)
 	_tag(Tags.GUARD_HARBOUR_OFFICE_SOBERING, 1)
 	_item(Bag.CrystalTeleportationOasis, 1)
@@ -25,7 +25,7 @@ func compute() -> void :
 	_item(Bag.ParcheminBarbeDrueDecoder, 2) # remplace le FruitDefendu
 	_item(Bag.CarteHungConnut, 1)
 	
-	#quete 3
+	#quete 3 : 
 	_item(Bag.Houblon, 1)
 	
 	self.text = "Score : "+str(value)
