e16a9c0e4982e58d69fca919034dbd938d2b1c3e
Kepka Ludovic <shionn@gmail.com>
2026-02-08 23:29:44 +0100

    nouveau dialog

--- scripts/tortuga/capitainerie/crystal_teleporteur.gd
+++ scripts/tortuga/capitainerie/crystal_teleporteur.gd
@@ -1,16 +1,17 @@
 extends "res://scripts/interactable.gd"
 
-func on_tag_change() -> void:
-	self.visible = tags.have(Tags.FORBID_FRUIT_SEARCH) and !bag.contain(Bag.CrystalTeleportationOasis)
-
 func on_item_change() -> void:	
-	self.visible = tags.have(Tags.FORBID_FRUIT_SEARCH) and !bag.contain(Bag.CrystalTeleportationOasis)
+	self.visible = !bag.contain(Bag.CrystalTeleportationOasis)
 
 func on_interact() -> void:
 	if not tags.have(Tags.FORBID_FRUIT_SEARCH_TELEPORT) :
-		gui.open_alert("Cristal étrange", "C’est quoi ce truc ? ça semble dangereux faut mieux ne pas y toucher.")
+		gui.open_dialog_next(Dialog.playerSay(player, "C’est quoi ce truc ? Ça semble dangereux faut mieux ne pas y toucher."))
 	else :
-		bag.loot(Bag.CrystalTeleportationOasis)
-		tags.remove(Tags.FORBID_FRUIT_SEARCH_TELEPORT)
-		gui.open_alert("Cristal étrange", "Ca doit être le cristal dont parlais Zakari.")
-#		self.visible = false
+		gui.open_dialog_next(Dialog.playerSay(player,"Ca doit être le cristal dont parlais Zakari.")
+			.on_close(_on_loot)
+		)
+
+func _on_loot():
+	bag.loot(Bag.CrystalTeleportationOasis)
+	tags.remove(Tags.FORBID_FRUIT_SEARCH_TELEPORT)
+	
